24215c312dde0531ace8f6b2103b57a79ce27b35
aya2705 <touicha.aya@gmail.com>
2025-03-28 02:54:24 +0000

    final version

--- script.js
+++
@@ -1,7 +0,0 @@
-const progress = document.querySelector(".progress");
-
-progress.addEventListener("click", () => {
-  const value = Math.floor(Math.random() * 100);
-  progress.style.setProperty("--progress", `${value}%`);
-});
-
